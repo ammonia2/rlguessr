b083cc31254b890bf40c0b9e4601078f60678496
luzair <m500.abdullah@gmail.com>
2024-07-29 16:32:24 +0500

    vercel config

---
+++ vercel.config.js
@@ -0,0 +1,25 @@
+{
+  "builds": [
+    {
+      "src": "index.html",
+      "use": "@vercel/static-build",
+      "config": {
+        "distDir": "dist"
+      }
+    },
+    {
+      "src": "api/**/*.js",
+      "use": "@vercel/node"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/api/(.*)",
+      "dest": "/api/$1"
+    },
+    {
+      "src": "/(.*)",
+      "dest": "/index.html"
+    }
+  ]
+}
