2b84133eb9d0bf12ab17d66e681c42b39db13fe5
luzair <m500.abdullah@gmail.com>
2024-07-30 17:36:42 +0500

    test url

--- api/teamOfDay.js
+++ api/teamOfDay.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 const { Pool } = pg;
 
-const databaseUrl = process.env.POSTRES_URL
+const databaseUrl = process.env.TEST_URL
 
 let teams = [
     { name: "Elevate", region: "APAC", rlcsLans: "3", yearJoined: "2021", winRate: "70.19", winnings:"94,164", active:"false", page:"https://liquipedia.net/rocketleague/Elevate" },
